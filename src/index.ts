9820726a804b94026507f7c523764b82f4e13fb7
Abdullah Al Mamun Roni <79703085+info-aamroni@users.noreply.github.com>
2026-03-17 21:02:26 +0600

    refactor ioc container bootstrapping and module-scoped middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
+import { OnboardProvider } from '@/app/onboard/onboard.provider.ts'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+OnboardProvider.register(app, '/api')
+
+app.get('/', (c) => c.text('Hello Hono!'))
 
 export default app
